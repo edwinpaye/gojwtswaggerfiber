f424bd7e41c36c592af9e0e00979ef1dd68d930d
edwinpaye <baratheonts@gmail.com>
2023-06-23 01:13:50 -0400

    jwt_generator fixed

--- pkg/utils/jwt_generator.go
+++ pkg/utils/jwt_generator.go
@@ -2,7 +2,6 @@ package utils
 
 import (
 	"os"
-	"strconv"
 	"time"
 
 	// "github.com/edwinpaye/gots/pkg/common/config"
@@ -17,14 +16,14 @@ func GenerateNewAccessToken() (string, error) {
 	// secret := c.JWTKey
 
 	// Set expires minutes count for secret key from .env file.
-	minutesCount, _ := strconv.Atoi(os.Getenv("JWT_SECRET_KEY_EXPIRE_MINUTES_COUNT"))
+	// minutesCount, _ := strconv.Atoi(os.Getenv("JWT_SECRET_KEY_EXPIRE_MINUTES_COUNT"))
 	// minutesCount, _ := strconv.Atoi(c.JWTKExpire)
 
 	// Create a new claims.
 	claims := jwt.MapClaims{}
 
 	// Set public claims:
-	claims["exp"] = time.Now().Add(time.Minute * time.Duration(minutesCount)).Unix()
+	claims["exp"] = time.Now().Add(time.Hour).Unix()
 
 	// Create a new JWT access token with claims.
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
